90f93ef444d17d2a7b57a753b20598d8154d49cf
nicoleneza <rwegoneza12@gmail.com>
2024-11-19 08:57:59 +0200

    authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.urls import reverse_lazy
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/login.html', next_page='accounts:login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('accounts:login')), name='logout'),
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
